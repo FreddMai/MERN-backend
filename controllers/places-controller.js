5307c88b3c0fcffa2503c49553b68735d2d59378
Freddy M <fredtein@gmail.com>
2020-06-24 17:56:54 +0300

    Using MapBox for converting address to coordinates

--- controllers/places-controller.js
+++ controllers/places-controller.js
@@ -3,6 +3,7 @@ const uuid = require("uuid");
 const { validationResult } = require("express-validator");
 
 const HttpError = require("../models/http-error");
+const getCoordsForAddress = require("../util/location");
 
 let DUMMY_PLACES = {
   id: "p1",
@@ -44,13 +45,20 @@ const getPlacesByUserId = (req, res, next) => {
   res.json({ places });
 };
 
-const createPlace = (req, res, next) => {
+const createPlace = async (req, res, next) => {
   const errros = validationResult(req);
   if (!errros.isEmpty()) {
-    throw new HttpError("Invalid inputs passed, please check your data.", 422);
+    next(new HttpError("Invalid inputs passed, please check your data.", 422));
   }
 
-  const { title, description, coordinates, address, creator } = req.body;
+  const { title, description, address, creator } = req.body;
+
+  let coordinates;
+  try {
+    coordinates = await getCoordsForAddress(address);
+  } catch (error) {
+    return next(err);
+  }
   const createdPlace = {
     id: uuid(),
     title,
