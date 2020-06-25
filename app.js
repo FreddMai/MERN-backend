a7d173d19e69c0b799add5d1ed3ce625da87a5d7
Freddy M <fredtein@gmail.com>
2020-06-25 10:40:07 +0300

    Creating the place schema using mongoose

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const placesRoutes = require("./routes/places-routes");
 const usersRoutes = require("./routes/users-routes");
 const HttpError = require("./models/http-error");
+const mongoConnect = require("./util/keys").mongoConnect;
 
 const app = express();
 
@@ -26,4 +28,11 @@ app.use((error, req, res, next) => {
   res.json({ message: error.message || "An unknown error occured!" });
 });
 
-app.listen(5000);
+mongoose
+  .connect(mongoConnect)
+  .then(() => {
+    app.listen(5000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
