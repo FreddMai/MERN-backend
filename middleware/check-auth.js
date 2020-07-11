197e38db03cc4ab145ce07d2e5b1968ae954ddae
Freddy M <fredtein@gmail.com>
2020-07-11 13:11:19 +0300

    Adding auth middleware with jwt

---
+++ middleware/check-auth.js
@@ -0,0 +1,22 @@
+const jwt = require("jsonwebtoken");
+
+const jwtSecretKey = require("./util/keys").jwtSecretKey;
+const HttpError = require("../models/http-error");
+
+module.exports = (req, res, next) => {
+  if (req.methods === "OPTIONS") {
+    return next();
+  }
+  try {
+    const token = req.headers.authorization.split(" ")[1];
+    if (!token) {
+      throw new Error("Authentocation failed!");
+    }
+    const decodedToken = jwt.verify(token, jwtSecretKey);
+    req.userData = { userId: decodedToken.userId };
+    next();
+  } catch (err) {
+    const error = new HttpError("Authentication Failed!", 401);
+    return next(error);
+  }
+};
